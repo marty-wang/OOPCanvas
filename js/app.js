b351ac5959c6bf684bcdce1edd66b3907e808d4e
Mo Wang <mo.hy.wang@gmail.com>
2011-04-20 15:15:58 -0700

    update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    
+
     var canvas = document.getElementById('oopcanvas');
     var oc = new OC(canvas, {
         'lineWidth': 4,
@@ -15,30 +15,32 @@ $(document).ready(function() {
     var i;
     var x, y, x1, y1;
 
-    for (i = 0; i <= num_width; i++) {
-        x = i * gutter;
-        y = 0;
-        x1 = x;
-        y1 = y + height;
-      
-        oc.drawLine(x, y, x1, y1);
-    }
+    $('#draw-button').click(function(evt) {
+        for (i = 0; i <= num_width; i++) {
+            x = i * gutter;
+            y = 0;
+            x1 = x;
+            y1 = y + height;
+          
+            oc.drawLine(x, y, x1, y1);
+        }
 
-    for (i = 0; i <= num_height; i++) {
-        y = i * gutter;
-        x = 0;
-        y1 = y;
-        x1 = x + width;
-      
-        oc.drawLine(x, y, x1, y1);
-    }
-    
-    oc.drawRect(100, 100, 100, 100, {
-        'fillStyle': 'red'
-    });
-    
-    oc.drawRect(100, 100, 100, 100, {
-        'rotation': Math.PI/4,
-        'fillStyle': 'blue'
+        for (i = 0; i <= num_height; i++) {
+            y = i * gutter;
+            x = 0;
+            y1 = y;
+            x1 = x + width;
+          
+            oc.drawLine(x, y, x1, y1);
+        }
+        
+        oc.drawRect(100, 100, 100, 100, {
+            'fillStyle': 'red'
+        });
+        
+        oc.drawRect(100, 100, 100, 100, {
+            'rotation': Math.PI/4,
+            'fillStyle': 'blue'
+        }); 
     });
 });
